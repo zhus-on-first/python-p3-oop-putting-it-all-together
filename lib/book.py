2bb09477698353bf7c7c577b7819ad2c2ef82bc1
Jia-Jia Zhu <135554427+zhus-on-first@users.noreply.github.com>
2023-09-13 09:50:48 -0700

    fun

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count) -> None:
+        self.title = title
+        self.page_count = page_count
+  
+
+    @property
+    def page_count(self):
+        return self._page_count
     
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, (float, int)):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
         
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size, condition="whatever") -> None:
+        self.brand = brand
+        self.size = size
+        self.condition = condition
+
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, value):
+        if isinstance(value, (float, int)):
+            self._size = value
+        else:
+            print("size must be an integer")
+    
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
